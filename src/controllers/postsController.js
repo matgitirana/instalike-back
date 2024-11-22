647f1e3f0601afed552a28a744fcbe77aaa55c49
Matheus Gitirana <matgitirana@gmail.com>
2024-11-21 22:47:07 -0300

    Aula 4

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -1,6 +1,36 @@
-import { getAllPosts } from '../models/postModel.js'
+import { createPost, getAllPosts } from '../models/postModel.js'
+import fs from 'fs'
 
 export async function listPosts(req, res) {
   const posts = await getAllPosts()
   res.status(200).json(posts)
+}
+
+export async function postNewPost(req, res) {
+  const newPost = req.body
+  try {
+    const createdPost = await createPost(newPost)
+    res.status(200).json(createdPost)
+  } catch (error) {
+    console.error(error.message)
+    res.status(500).json({ Error: 'Request failed' })
+  }
+}
+
+export async function uploadImage(req, res) {
+  const newPost = {
+    description: '',
+    imgUrl: req.file.originalname,
+    alt: '',
+  }
+
+  try {
+    const createdPost = await createPost(newPost)
+    const updatedImage = `uploads/${createdPost.insertedId}.png`
+    fs.renameSync(req.file.path, updatedImage)
+    res.status(200).json(createdPost)
+  } catch (error) {
+    console.error(error.message)
+    res.status(500).json({ Erro: 'Request failed' })
+  }
 }
--- src/routes/postRoutes.js
+++ src/routes/postRoutes.js
@@ -1,10 +1,28 @@
 import express from 'express'
-import { listPosts } from '../controllers/postsController.js'
+import {
+  listPosts,
+  postNewPost,
+  uploadImage,
+} from '../controllers/postsController.js'
+import multer from 'multer'
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname)
+  },
+})
+
+const upload = multer({ dest: './uploads', storage })
 
 const routes = (app) => {
   app.use(express.json())
 
   app.get('/posts', listPosts)
+  app.post('/posts', postNewPost)
+  app.post('/upload', upload.single('image'), uploadImage)
 }
 
 export default routes
